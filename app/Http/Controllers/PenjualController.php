8c4da4f3209b5216b26a3955e92a7b4cffc28080
husinassegaff <husinassegaff15@gmail.com>
2021-12-05 00:18:43 +0700

    update: penjualcontroller

--- app/Http/Controllers/PenjualController.php
+++ app/Http/Controllers/PenjualController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\SellerUser;
+use App\Models\SellLaptop;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -16,10 +17,6 @@ class PenjualController extends Controller
     public function register()
     {
         return view('dashboard.penjual.register');
-    }
-
-    public function dashboard(){
-        return view('dashboard.penjual.dashboard');
     }
 
     public function store(Request $request)
@@ -61,5 +58,55 @@ class PenjualController extends Controller
 
         $request->session()->regenerateToken();
         return redirect()->route('beranda');
+    }
+
+    public function dashboard(){
+        return view('dashboard.penjual.dashboard',[
+            'laptop' => SellLaptop::where('seller_user_id', Auth::guard('seller_user')->id())->get(),
+            'laptop_waiting' => SellLaptop::where('seller_user_id', Auth::guard('seller_user')->id())->where('sell_laptop_status', 0)->get(),
+            'laptop_accepted' => SellLaptop::where('seller_user_id', Auth::guard('seller_user')->id())->where('sell_laptop_status', 1)->get(),
+            'laptop_rejected' => SellLaptop::where('seller_user_id', Auth::guard('seller_user')->id())->where('sell_laptop_status', 2)->get(),
+        ]);
+    }
+
+    public function offers(){
+        return view('dashboard.penjual.dashboardoffer',[
+            'offers' => SellLaptop::where('seller_user_id', Auth::guard('seller_user')->id())->where('sell_laptop_status', 0)->get(),
+        ]);
+    }
+
+    public function offersaccepted(){
+        return view('dashboard.penjual.dashboardofferaccepted',[
+            'offers' => SellLaptop::where('seller_user_id', Auth::guard('seller_user')->id())->where('sell_laptop_status', 1)->get(),
+        ]);
+    }
+
+    public function offersrejected(){
+        return view('dashboard.penjual.dashboardofferrejected',[
+            'offers' => SellLaptop::where('seller_user_id', Auth::guard('seller_user')->id())->where('sell_laptop_status', 2)->get(),
+        ]);
+    }
+
+    public function addlaptop(){
+        return view('dashboard.penjual.dashboardaddlaptop');
+    }
+
+    public function storelaptop(Request $request){
+        $validatedData = $request->validate([
+            'sell_laptop_name' => 'required',
+            'sell_laptop_brand' => 'required',
+            'sell_laptop_type' => 'required',
+            'sell_laptop_desc' => 'required',
+            'sell_laptop_condition' => 'required',
+            'sell_laptop_weight' => 'required|numeric',
+            'sell_laptop_price' => 'required|numeric',
+            'sell_laptop_usage_time' => 'required|numeric',
+        ]);
+
+        $validatedData['seller_user_id'] = Auth::guard('seller_user')->id();
+
+        SellLaptop::create($validatedData);
+
+        return redirect('/dashboard')->with('success', 'Laptop has been added. Please wait for admin to accept your offer');
     }
 }
