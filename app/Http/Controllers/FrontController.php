86bfdbc110fa3fabd322442ad1897a343639963c
mirzaq19 <mirzaqarjap@gmail.com>
2021-12-05 14:11:39 +0700

    add: list product by brand

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -20,4 +20,69 @@ class FrontController extends Controller
             'laptop' => $laptop,
         ]);
     }
+
+    public function listasus()
+    {
+        $laptops = Laptop::where('laptop_brand','Asus')->get();
+        return view('front.list',[
+            'title' => 'Asus',
+            'laptops' => $laptops,
+        ]);
+    }
+    public function listhp()
+    {
+        $laptops = Laptop::where('laptop_brand','HP')->get();
+        return view('front.list',[
+            'title' => 'HP',
+            'laptops' => $laptops,
+        ]);
+    }
+    public function listlenovo()
+    {
+        $laptops = Laptop::where('laptop_brand','Lenovo')->get();
+        return view('front.list',[
+            'title' => 'Lenovo',
+            'laptops' => $laptops,
+        ]);
+    }
+    public function listdell()
+    {
+        $laptops = Laptop::where('laptop_brand','Dell')->get();
+        return view('front.list',[
+            'title' => 'Dell',
+            'laptops' => $laptops,
+        ]);
+    }
+    public function listacer()
+    {
+        $laptops = Laptop::where('laptop_brand','Acer')->get();
+        return view('front.list',[
+            'title' => 'Acer',
+            'laptops' => $laptops,
+        ]);
+    }
+    public function listapple()
+    {
+        $laptops = Laptop::where('laptop_brand','Apple')->get();
+        return view('front.list',[
+            'title' => 'Apple',
+            'laptops' => $laptops,
+        ]);
+    }
+    public function listtoshiba()
+    {
+        $laptops = Laptop::where('laptop_brand','Toshiba')->get();
+        return view('front.list',[
+            'title' => 'Toshiba',
+            'laptops' => $laptops,
+        ]);
+    }
+    public function listaxioo()
+    {
+        $laptops = Laptop::where('laptop_brand','Axioo')->get();
+        return view('front.list',[
+            'title' => 'Axioo',
+            'laptops' => $laptops,
+        ]);
+    }
 }
